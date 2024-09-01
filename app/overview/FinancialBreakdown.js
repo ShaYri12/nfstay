8576258ef2484d6ec794d21cc988cb520e4af80e
ShaYri12 <sharjeelb60@gmail.com>
2024-09-01 05:38:29 +0500

    details last 2 sections remaining

--- app/overview/FinancialBreakdown.js
+++ app/overview/FinancialBreakdown.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const FinancialBreakdown = () => {
   return (
-    <div className=" p-[25px]">
-      <h1 className="text-[32px] leading-[40.32px] tracking-[-2%] font-[700] text-center text-primary-color mb-8">
+    <div className="px-[25px] pb-[60px]">
+      <h1 className="text-[32px] leading-[40.32px] tracking-[-2%] font-[700] text-center text-primary-color mb-[40px]">
         Financial Breakdown
       </h1>
-      <div className="flex flex-col md:flex-row gap-[30px] justify-center px-[25px]">
+      <div className="flex flex-col md:flex-row gap-[30px] justify-center items-start">
         {/* Transaction Card */}
-        <div className="bg-[#F7F6FF] shadow-md rounded-lg p-6 w-full md:w-1/2">
-          <div className="flex justify-between items-center mb-4">
+        <div className="bg-[#F7F6FF] rounded-[14px] p-[25px] w-full md:w-1/2">
+          <div className="flex justify-between items-center pb-[20px] border-b border-opacity-[0.1] border-b-[#000000]">
             <div className="flex items-center">
               <div className="bg-white p-[9px] rounded-[10px] mr-2">
                 {/* Placeholder for Icon */}
@@ -18,39 +18,120 @@ const FinancialBreakdown = () => {
                 </span>
               </div>
               <div>
-                <h2 className="text-lg font-semibold text-purple-900">
+                <h2 className="text-[16px] font-[600] leading-[22.68px] text-primary-color">
                   Transaction
                 </h2>
-                <a href="#" className="text-sm text-blue-500 underline">
+                <a
+                  href="#"
+                  className="text-[11px] font-[400] text-[#954AFC] leading-[14px]"
+                >
                   Details
                 </a>
               </div>
             </div>
-            <div className="text-gray-500 text-sm">Last Update: 2 days ago</div>
+            <div className="flex items-center gap-[12px]">
+              <span className="text-[11px] font-[500] leading-[16px] text-primary-color opacity-[0.5]">
+                Last Update: 2 days ago
+              </span>
+              <div className="w-[34px] h-[34px] border border-[#0000001F] rounded-[7px] flex items-center justify-center">
+                <div className="h-[1.5px] w-[12px] bg-primary-color"></div>
+              </div>
+            </div>
           </div>
-          <ul className="text-gray-700 space-y-1">
-            <li className="flex justify-between">
-              <span>Property Listing Price</span>
-              <span>USD 728,000</span>
+          <ul className="text-primary-color flex flex-col gap-[14px] pt-[20px]">
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Property Listing Price
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 728,000
+              </span>
             </li>
-            <li className="flex justify-between font-semibold">
-              <span>Transaction Costs</span>
-              <span>USD 72,000</span>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                Transaction Costs
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 72,000
+              </span>
             </li>
-            {/* Add other list items in a similar manner */}
-            <li className="flex justify-between text-purple-600 font-semibold">
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Title Search & Insurance (0.5% - 1%)
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 7,200
+              </span>
+            </li>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Recording Fees (0.2% - 0.5%)
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 3,600
+              </span>
+            </li>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Legal Fees (0.5% - 1%)
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 7,200
+              </span>
+            </li>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Taxes (1%)
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 7,200
+              </span>
+            </li>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Broker Fee (3%)
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 21,600
+              </span>
+            </li>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Renovation Costs (2%)
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 14,400
+              </span>
+            </li>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Metropoly fee (1.5%)
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 10,800
+              </span>
+            </li>
+            <li className="flex justify-between text-[#954AFC] font-[700] text-[15px] leading-[22.68px] gap-1">
               <span>Total Acquisition Cost</span>
               <span>USD 7,200</span>
             </li>
-            <li className="flex justify-between">
-              <span>Total Number of NFTs</span>
-              <span>1,000</span>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Total Number of NFTs
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                1,000
+              </span>
             </li>
-            <li className="flex justify-between">
-              <span>Value of One NFT</span>
-              <span>1/1000</span>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Value of One NFT
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                1/1000
+              </span>
             </li>
-            <li className="flex justify-between text-purple-600 font-semibold">
+            <li className="flex justify-between text-[#954AFC] font-[700] text-[15px] leading-[22.68px] gap-1">
               <span>Mint Price Per NFT</span>
               <span>USD 800</span>
             </li>
@@ -58,43 +139,96 @@ const FinancialBreakdown = () => {
         </div>
 
         {/* Rental Card */}
-        <div className="bg-white shadow-md rounded-lg p-6 w-full md:w-1/2">
-          <div className="flex justify-between items-center mb-4">
+        <div className="bg-[#F7F6FF] rounded-[14px] p-[25px] w-full md:w-1/2">
+          <div className="flex justify-between items-center pb-[20px] border-b border-opacity-[0.1] border-b-[#000000]">
             <div className="flex items-center">
-              <div className="bg-purple-100 p-2 rounded-full mr-2">
+              <div className="bg-white p-[9px] rounded-[10px] mr-2">
                 {/* Placeholder for Icon */}
-                <svg
-                  className="w-6 h-6 text-purple-600"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 0L11.297 8.187H0L0.702 0H12Z" />
-                </svg>
+                <span className="w-[22px] h-[22px]">
+                  <img src="/assets/icons/file-icon.svg" alt="file" />
+                </span>
               </div>
               <div>
-                <h2 className="text-lg font-semibold text-purple-900">
+                <h2 className="text-[16px] font-[600] leading-[22.68px] text-primary-color">
                   Rental
                 </h2>
-                <a href="#" className="text-sm text-blue-500 underline">
+                <a
+                  href="#"
+                  className="text-[11px] font-[400] text-[#954AFC] leading-[14px]"
+                >
                   Breakdown
                 </a>
               </div>
             </div>
-            <div className="text-gray-500 text-sm">Last Update: 2 days ago</div>
+            <div className="flex items-center gap-[12px]">
+              <span className="text-[11px] font-[500] leading-[16px] text-primary-color opacity-[0.5]">
+                Last Update: 2 days ago
+              </span>
+              <div className="w-[34px] h-[34px] border border-[#0000001F] rounded-[7px] flex items-center justify-center">
+                <div className="h-[1.5px] w-[12px] bg-primary-color"></div>
+              </div>
+            </div>
           </div>
-          <ul className="text-gray-700 space-y-1">
-            <li className="flex justify-between">
-              <span>Gross Yield</span>
-              <span>10.32 %</span>
+          <ul className="text-primary-color flex flex-col gap-[14px] pt-[20px]">
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Gross Yield
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                10.32 %
+              </span>
             </li>
-            <li className="flex justify-between">
-              <span>Gross Rent Per Year</span>
-              <span>USD 88,560</span>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Gross Rent Per Year
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 88,560
+              </span>
             </li>
-            {/* Add other list items in a similar manner */}
-            <li className="flex justify-between text-purple-600 font-semibold">
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Service Charges (10%)
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 8,856
+              </span>
+            </li>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Property Management (15%)
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 88,560
+              </span>
+            </li>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Property Insurance
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 11,937
+              </span>
+            </li>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Maintenance Charges (10%)
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                USD 11,937
+              </span>
+            </li>
+            <li className="flex justify-between gap-1">
+              <span className="text-[13px] font-[500] leading-[18.9px] opacity-[0.5]">
+                Net Dividend Yield
+              </span>
+              <span className="text-[13px] font-[600] leading-[18.9px]">
+                5.32 %
+              </span>
+            </li>
+            <li className="flex justify-between text-[#954AFC] font-[700] text-[15px] leading-[22.68px] gap-1">
               <span>Annual Rental Income</span>
-              <span>USD 45,752</span>
+              <span>USD 45,752</span>
             </li>
           </ul>
         </div>
